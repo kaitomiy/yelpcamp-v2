2a22db3835cff4bbaa3848b0018422155b929415
Kaito Mitomi <kaitomiy@gmail.com>
2020-10-18 20:44:31 +0900

    add joi validation

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 const Campground = require('./models/campground');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
+const { campgroundSchema } = require('./schemas');
 
 // MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
@@ -30,6 +31,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
+
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -52,9 +63,15 @@ app.get('/campgrounds/new', (req, res) => {
 // Create a new post
 app.post(
   '/campgrounds',
+  validateCampground,
   catchAsync(async (req, res) => {
-    if (!req.body.campground)
-      throw new ExpressError('Invalid Campground Data', 400);
+    // if (!req.body.campground)
+    //   throw new ExpressError('Invalid Campground Data', 400);
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+      const msg = error.details.map((el) => el.message).join(',');
+      throw new ExpressError(msg, 400);
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -82,6 +99,7 @@ app.get(
 // Update a post
 app.put(
   '/campgrounds/:id',
+  validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {
