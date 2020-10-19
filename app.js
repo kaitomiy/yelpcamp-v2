54acea48d6f08e3684eba13ac0440a3ccab5613b
Kaito Mitomi <kaitomiy@gmail.com>
2020-10-19 14:44:49 +0900

    refactor reviews routes

--- app.js
+++ app.js
@@ -7,13 +7,10 @@ const morgan = require('morgan');
 const ejsMate = require('ejs-mate');
 require('dotenv').config();
 
-const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
-const Campground = require('./models/campground');
-const Review = require('./models/review');
-const { reviewSchema } = require('./schemas');
 
 const campgroundsRoute = require('./routes/campgrounds');
+const reviewsRoute = require('./routes/reviews');
 
 // MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
@@ -36,46 +33,12 @@ app.use(methodOverride('_method'));
 app.use(morgan('dev'));
 
 app.use('/campgrounds', campgroundsRoute);
+app.use('/campgrounds/:id/reviews', reviewsRoute);
 
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(',');
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
-
+// Landing page
 app.get('/', (req, res) => {
   res.render('home');
 });
-
-// REVIEW ROUTE
-// Create a new review
-app.post(
-  '/campgrounds/:id/reviews',
-  validateReview,
-  catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
-
-// Delete a review
-app.delete(
-  '/campgrounds/:id/reviews/:reviewId',
-  catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-  })
-);
 
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page not found', 404));
