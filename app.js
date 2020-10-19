154c9174c8ce52f0f9f6a3a04ea29f33cd1c14e0
Kaito Mitomi <kaitomiy@gmail.com>
2020-10-19 12:00:17 +0900

    add delete a review

--- app.js
+++ app.js
@@ -139,6 +139,17 @@ app.post(
   })
 );
 
+// Delete a review
+app.delete(
+  '/campgrounds/:id/reviews/:reviewId',
+  catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+  })
+);
+
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page not found', 404));
 });
