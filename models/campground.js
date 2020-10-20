0531fbacac014fc16830a6a833ee1b7176d59fd9
Kaito Mitomi <kaitomiy@gmail.com>
2020-10-21 08:51:52 +0900

    add custom popup

--- models/campground.js
+++ models/campground.js
@@ -1,3 +1,4 @@
+const { string } = require('joi');
 const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
@@ -11,33 +12,44 @@ ImageSchema.virtual('thumbnail').get(function () {
   return this.url.replace('/upload', '/upload/w_200');
 });
 
-const CampgroundSchema = new Schema({
-  title: String,
-  images: [ImageSchema],
-  geometry: {
-    type: {
-      type: String,
-      enum: ['Point'],
-      required: true,
+const opts = { toJSON: { virtuals: true } };
+
+const CampgroundSchema = new Schema(
+  {
+    title: String,
+    images: [ImageSchema],
+    geometry: {
+      type: {
+        type: String,
+        enum: ['Point'],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
-    coordinates: {
-      type: [Number],
-      required: true,
-    },
-  },
-  price: Number,
-  description: String,
-  location: String,
-  reviews: [
-    {
+    price: Number,
+    description: String,
+    location: String,
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Review',
+      },
+    ],
+    author: {
       type: Schema.Types.ObjectId,
-      ref: 'Review',
+      ref: 'User',
     },
-  ],
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
   },
+  opts
+);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+  return `<strong><a href='/campgrounds/${
+    this._id
+  }'>${this.title}</a></strong><p>${this.description.substring(0, 20)}...</p>`;
 });
 
 CampgroundSchema.post('findOneAndDelete', async (doc) => {
