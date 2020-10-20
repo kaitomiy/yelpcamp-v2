8530d66b8712914b65931bcea2c0129772b4a606
Kaito Mitomi <kaitomiy@gmail.com>
2020-10-20 16:06:38 +0900

    add uploading more images and file input

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -54,6 +54,12 @@ module.exports.updateCampground = async (req, res) => {
   const campground = await Campground.findByIdAndUpdate(id, {
     ...req.body.campground,
   });
+  const imgs = req.files.map((file) => ({
+    url: file.path,
+    filename: file.filename,
+  }));
+  campground.images.push(...imgs);
+  await campground.save();
   req.flash('success', 'Successfully updated a campground!!');
   res.redirect(`/campgrounds/${campground._id}`);
 };
